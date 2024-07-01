c37c8ad0d5a9c5c013c8288c8d70aadef7d99ac3
Claudio-Emmolo <113107618+Claudio-Emmolo@users.noreply.github.com>
2024-07-01 16:55:37 +0200

    Add image aspect ratio

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,37 @@ function setDefaultSelectValue() {
         selectElement.value = "Default";
     }
 }
-document.addEventListener("DOMContentLoaded", setDefaultSelectValue);
+
+
+function addAspectRatioToImage() {
+    const novaImages = document.querySelectorAll("#nova button img");
+
+    novaImages.forEach((image) => {
+            const width = image.naturalWidth;
+            const height = image.naturalHeight;
+
+            const aspectRatio = document.createElement("div");
+            Object.assign(aspectRatio.style, {
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                overflow: "hidden",
+                width: `${height}px`,
+                height: `${height}px`,
+                border: "2px solid lime"
+            });
+
+            // Aggiungi la div all'interno del pulsante
+            image.parentElement.classList.add("relative");
+            image.parentElement.appendChild(aspectRatio);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    setDefaultSelectValue()
+    setTimeout(() => {
+        console.log("DOM fully loaded and parsed");
+        addAspectRatioToImage();
+    } , 100);
+})
